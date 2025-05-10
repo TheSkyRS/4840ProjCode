80694fd2542d82474cc02d658064483280666ac1
CaoYang1789 <yc4535@columbia.edu>
2025-05-10 14:37:51 -0400

    cy

--- sw/src/main.c
+++ sw/src/main.c
@@ -24,10 +24,7 @@ int main()
     player_init(&players[1], 200, 400, 2, 3, PLAYER_WATERGIRL);
 
     unsigned col = 0, row = 0;
-    for (uint8_t i = 0; i < 44; i++)
-    {
-        write_sprite(i, 1, 0, (i % 11) * 32, (i / 11) * 32, i);
-    }
+    write_sprite(1, 1, 0, 32, 32, 1);
 
     // while (1)
     // {
