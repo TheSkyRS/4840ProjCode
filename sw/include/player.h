b1626e81bf7290a7fbb7db60abde9fb1acb84f4c
CaoYang1789 <yc4535@columbia.edu>
2025-05-13 19:35:32 -0400

    cy

--- sw/include/player.h
+++ sw/include/player.h
@@ -15,7 +15,7 @@ int get_frame_count(player_t *p, bool is_upper);
 
 // 每帧调用流程
 void player_handle_input(player_t *p, int player_index);
-void player_update_physics(player_t *p);
+bool player_update_physics(player_t *p);
 void player_check_collision(player_t *p);
 void player_update_sprite(player_t *p);
 void adjust_to_slope_y(player_t *p);
